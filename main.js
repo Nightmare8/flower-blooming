cff98d754d4ea574de278ec18e89c3aa1b077004
JP Caniguante <juan.caniguante@etpay.com>
2025-01-05 12:54:28 -0300

    feat: some updates

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+document.addEventListener("DOMContentLoaded", () => {
+	onload = () => {
+		const c = setTimeout(() => {
+			document.body.classList.remove("not-loaded");
+			clearTimeout(c);
+		}, 1000);
+	};
+
+	const phrases = [
+		"Eres la mejor Enfermera 🐈‍⬛",
+		"Estefanía, eres maravillosa 🌹",
+		"Siempre estaré para ti 💫",
+		"Puedes con todooo 💪🏼",
+		"Leona licenciada oficial 🦁",
+		"Te quiero mucho 💕",
+		"Te mereces toditooo 🌟",
+	];
+
+	const title = document.getElementById("title");
+
+	const index = Math.floor(Math.random() * phrases.length);
+
+	title.innerHTML = phrases[index];
+});
